cc048e369575b39351fea4c1cc2ee098dc7128ef
BuiBang0911 <bui09bang11@gmail.com>
2026-02-06 14:09:04 +0700

    update db EnumRole

---
+++ be/Infrastructure/Data/Migrations/20260206070315_UpdateEnumForRole.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEnumForRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+        ALTER TABLE "Users"
+        ALTER COLUMN "Role" TYPE integer
+        USING ("Role"::integer);
+    """);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+        ALTER TABLE "Users"
+        ALTER COLUMN "Role" TYPE text
+        USING ("Role"::text);
+    """);
+        }
+    }
+}
