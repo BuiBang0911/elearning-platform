7f00aac7f226685224f1289faf59bf5c1cfc20fe
BuiBang0911 <bui09bang11@gmail.com>
2026-02-06 16:49:05 +0700

    update allow null for lecture in course

--- be/Infrastructure/Entities/Course.cs
+++ be/Infrastructure/Entities/Course.cs
@@ -10,11 +10,11 @@ namespace Infrastructure.Entities
     {
         public string Title { get; set; }
         public string Description { get; set; }
-        public int LecturerId { get; set; }
+        public int? LecturerId { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
         // Navigation properties
-        public virtual User Lecturer { get; set; }
+        public virtual User? Lecturer { get; set; }
         public virtual ICollection<Enrollment> Enrollments { get; set; }
         public virtual ICollection<Lesson> Lessons { get; set; }
     }
