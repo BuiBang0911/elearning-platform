eedc97d333b1d0451fa3ce1e7670d7b8c78c4ee0
BuiBang0911 <bui09bang11@gmail.com>
2026-02-11 15:10:26 +0700

    update logout

--- be/Web/Controllers/AccountController.cs
+++ be/Web/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using ApplicationCore.Services.Auth;
+using ApplicationCore.Services.Cache;
 using ApplicationCore.Services.Token;
 using ApplicationCore.Services.Users;
+using AutoMapper;
 using Infrastructure.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
@@ -17,11 +19,16 @@ namespace Web.Controllers
         private readonly JwtService _jwtService;
         private readonly IAuthService _authService;
         private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+        private readonly ICacheService _cacheService;
 
-        public AccountController(JwtService jwtService, IUserService userService)
+        public AccountController(JwtService jwtService, IAuthService authService, IUserService userService, ICacheService cacheService, IMapper mapper)
         {
             _userService = userService;
             _jwtService = jwtService;
+            _mapper = mapper;
+            _authService = authService;
+            _cacheService = cacheService;
         }
 
         [HttpPost("login")]
@@ -60,16 +67,35 @@ namespace Web.Controllers
         }
 
         [Authorize]
-        [HttpPost]
+        [HttpPost("logout")]
         public async Task<IActionResult> Logout()
         {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            if (userId == null) return BadRequest();
-            var user = await _userService.FirstOrDefaultAsync(x => x.Id.ToString() == userId);
+
+            foreach (var c in User.Claims)
+            {
+                Console.WriteLine($"{c.Type} = {c.Value}");
+            }
+            var userId = _authService.UserId;
+            var jti = _authService.Jti;
+
+            if (userId == null || jti == null) return BadRequest();
+            var user = await _userService.FirstOrDefaultAsync(x => x.Id == userId);
             if (user == null) return BadRequest();
             user.RefreshToken = null;
             user.RefreshTokenExpiry = null;
             await _userService.UpdateAsync(user);
+
+            var expClaim = User.FindFirst("exp")?.Value;
+            if (long.TryParse(expClaim, out long expUnix))
+            {
+                var expiryDate = DateTimeOffset.FromUnixTimeSeconds(expUnix);
+                var timeUntilExpiry = expiryDate - DateTimeOffset.UtcNow;
+
+                if (timeUntilExpiry > TimeSpan.Zero)
+                {
+                    await _cacheService.SetAsync(jti, "revoked", timeUntilExpiry);
+                }
+            }
             return Ok();
         }
 
@@ -113,6 +139,18 @@ namespace Web.Controllers
             user.Role = UserRole.Lecturer;
             await _userService.UpdateAsync(user);
             return Ok();
+        }
+
+        [Authorize]
+        [HttpGet("get-me")]
+        public async Task<IActionResult> GetMe()
+        {
+            var userId = _authService.UserId;
+            if (userId == null) return BadRequest();
+            var user = await _userService.FirstOrDefaultAsync(x => x.Id == userId);
+            if (user == null) return BadRequest();
+            var res = _mapper.Map<UserResponse>(user);
+            return Ok(res);
         }
     }
 }
